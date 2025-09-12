69a5828cc3c9f84c70a827257d9ec757e7d4662f
cakmaak <yusuf612844@gmail.com>
2025-09-13 01:17:46 +0300

    updsecconfig6

--- src/main/java/com/ai_note_api/ainoteapi/Config/SecurityConfig.java
+++ src/main/java/com/ai_note_api/ainoteapi/Config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import com.ai_note_api.ainoteapi.Security.JwtAuthFilter; 
 
@@ -46,6 +47,13 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    
+
+@Bean
+public ForwardedHeaderFilter forwardedHeaderFilter() {
+    return new ForwardedHeaderFilter();
+}
+    
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
